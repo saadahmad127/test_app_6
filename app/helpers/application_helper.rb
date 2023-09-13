40731a6bb439d9ef44bac54cb999596f128591f7
Saad Ahmed <saadahmadbhatti127@gmail.com>
2023-09-13 17:59:25 +0500

    final commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,7 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
+
+  #methods defined here not accessible to controllers but only to views
+
 end
